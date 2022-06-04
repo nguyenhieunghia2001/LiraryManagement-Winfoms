8e3c7adc91d2b65411432459628789d524506e00
Phu Dinh <phu.dinh@inceptionlabs.com.vn>
2022-06-04 20:38:03 +0700

    fix confict

---
+++ LiraryManagerment/Views/CreateStaff.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace LiraryManagerment.Views
+{
+    partial class CreateStaff
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.c_s_lbHovaTen = new System.Windows.Forms.Label();
+            this.cs_tb_HovaTen = new System.Windows.Forms.TextBox();
+            this.cs_tb_SDT = new System.Windows.Forms.TextBox();
+            this.cs_lb_Phone = new System.Windows.Forms.Label();
+            this.cs_lb_Birthday = new System.Windows.Forms.Label();
+            this.cs_lb_BoPhan = new System.Windows.Forms.Label();
+            this.cs_cb_BoPhan = new System.Windows.Forms.ComboBox();
+            this.cs_lb_BangCap = new System.Windows.Forms.Label();
+            this.cs_cb_BangCap = new System.Windows.Forms.ComboBox();
+            this.cs_lb_ChucVu = new System.Windows.Forms.Label();
+            this.cs_cb_ChucVu = new System.Windows.Forms.ComboBox();
+            this.cs_lb_DiaChi = new System.Windows.Forms.Label();
+            this.cs_tb_DiaChi = new System.Windows.Forms.TextBox();
+            this.cs_btn_XacNhan = new System.Windows.Forms.Button();
+            this.cs_btn_Huy = new System.Windows.Forms.Button();
+            this.cs_dtp_NgaySinh = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbTitle.Location = new System.Drawing.Point(351, 33);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(292, 46);
+            this.lbTitle.TabIndex = 1;
+            this.lbTitle.Text = "Hồ Sơ Nhân Viên";
+            // 
+            // c_s_lbHovaTen
+            // 
+            this.c_s_lbHovaTen.AutoSize = true;
+            this.c_s_lbHovaTen.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.c_s_lbHovaTen.Location = new System.Drawing.Point(76, 134);
+            this.c_s_lbHovaTen.Name = "c_s_lbHovaTen";
+            this.c_s_lbHovaTen.Size = new System.Drawing.Size(97, 28);
+            this.c_s_lbHovaTen.TabIndex = 2;
+            this.c_s_lbHovaTen.Text = "Họ và Tên";
+            // 
+            // cs_tb_HovaTen
+            // 
+            this.cs_tb_HovaTen.Location = new System.Drawing.Point(179, 138);
+            this.cs_tb_HovaTen.Name = "cs_tb_HovaTen";
+            this.cs_tb_HovaTen.Size = new System.Drawing.Size(245, 27);
+            this.cs_tb_HovaTen.TabIndex = 3;
+            // 
+            // cs_tb_SDT
+            // 
+            this.cs_tb_SDT.Location = new System.Drawing.Point(666, 138);
+            this.cs_tb_SDT.Name = "cs_tb_SDT";
+            this.cs_tb_SDT.Size = new System.Drawing.Size(245, 27);
+            this.cs_tb_SDT.TabIndex = 4;
+            // 
+            // cs_lb_Phone
+            // 
+            this.cs_lb_Phone.AutoSize = true;
+            this.cs_lb_Phone.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cs_lb_Phone.Location = new System.Drawing.Point(527, 137);
+            this.cs_lb_Phone.Name = "cs_lb_Phone";
+            this.cs_lb_Phone.Size = new System.Drawing.Size(133, 28);
+            this.cs_lb_Phone.TabIndex = 5;
+            this.cs_lb_Phone.Text = "Số Điện Thoại";
+            // 
+            // cs_lb_Birthday
+            // 
+            this.cs_lb_Birthday.AutoSize = true;
+            this.cs_lb_Birthday.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cs_lb_Birthday.Location = new System.Drawing.Point(60, 220);
+            this.cs_lb_Birthday.Name = "cs_lb_Birthday";
+            this.cs_lb_Birthday.Size = new System.Drawing.Size(102, 28);
+            this.cs_lb_Birthday.TabIndex = 6;
+            this.cs_lb_Birthday.Text = "Ngày Sinh";
+            // 
+            // cs_lb_BoPhan
+            // 
+            this.cs_lb_BoPhan.AutoSize = true;
+            this.cs_lb_BoPhan.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cs_lb_BoPhan.Location = new System.Drawing.Point(577, 218);
+            this.cs_lb_BoPhan.Name = "cs_lb_BoPhan";
+            this.cs_lb_BoPhan.Size = new System.Drawing.Size(83, 28);
+            this.cs_lb_BoPhan.TabIndex = 8;
+            this.cs_lb_BoPhan.Text = "Bộ Phận";
+            // 
+            // cs_cb_BoPhan
+            // 
+            this.cs_cb_BoPhan.FormattingEnabled = true;
+            this.cs_cb_BoPhan.Location = new System.Drawing.Point(666, 218);
+            this.cs_cb_BoPhan.Name = "cs_cb_BoPhan";
+            this.cs_cb_BoPhan.Size = new System.Drawing.Size(245, 28);
+            this.cs_cb_BoPhan.TabIndex = 9;
+            // 
+            // cs_lb_BangCap
+            // 
+            this.cs_lb_BangCap.AutoSize = true;
+            this.cs_lb_BangCap.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cs_lb_BangCap.Location = new System.Drawing.Point(72, 317);
+            this.cs_lb_BangCap.Name = "cs_lb_BangCap";
+            this.cs_lb_BangCap.Size = new System.Drawing.Size(95, 28);
+            this.cs_lb_BangCap.TabIndex = 10;
+            this.cs_lb_BangCap.Text = "Bằng Cấp";
+            // 
+            // cs_cb_BangCap
+            // 
+            this.cs_cb_BangCap.FormattingEnabled = true;
+            this.cs_cb_BangCap.Location = new System.Drawing.Point(179, 317);
+            this.cs_cb_BangCap.Name = "cs_cb_BangCap";
+            this.cs_cb_BangCap.Size = new System.Drawing.Size(245, 28);
+            this.cs_cb_BangCap.TabIndex = 11;
+            // 
+            // cs_lb_ChucVu
+            // 
+            this.cs_lb_ChucVu.AutoSize = true;
+            this.cs_lb_ChucVu.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cs_lb_ChucVu.Location = new System.Drawing.Point(565, 313);
+            this.cs_lb_ChucVu.Name = "cs_lb_ChucVu";
+            this.cs_lb_ChucVu.Size = new System.Drawing.Size(84, 28);
+            this.cs_lb_ChucVu.TabIndex = 12;
+            this.cs_lb_ChucVu.Text = "Chức Vụ";
+            // 
+            // cs_cb_ChucVu
+            // 
+            this.cs_cb_ChucVu.FormattingEnabled = true;
+            this.cs_cb_ChucVu.Location = new System.Drawing.Point(666, 313);
+            this.cs_cb_ChucVu.Name = "cs_cb_ChucVu";
+            this.cs_cb_ChucVu.Size = new System.Drawing.Size(245, 28);
+            this.cs_cb_ChucVu.TabIndex = 13;
+            // 
+            // cs_lb_DiaChi
+            // 
+            this.cs_lb_DiaChi.AutoSize = true;
+            this.cs_lb_DiaChi.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cs_lb_DiaChi.Location = new System.Drawing.Point(72, 420);
+            this.cs_lb_DiaChi.Name = "cs_lb_DiaChi";
+            this.cs_lb_DiaChi.Size = new System.Drawing.Size(74, 28);
+            this.cs_lb_DiaChi.TabIndex = 14;
+            this.cs_lb_DiaChi.Text = "Địa Chỉ";
+            // 
+            // cs_tb_DiaChi
+            // 
+            this.cs_tb_DiaChi.Location = new System.Drawing.Point(179, 421);
+            this.cs_tb_DiaChi.Name = "cs_tb_DiaChi";
+            this.cs_tb_DiaChi.Size = new System.Drawing.Size(732, 27);
+            this.cs_tb_DiaChi.TabIndex = 15;
+            // 
+            // cs_btn_XacNhan
+            // 
+            this.cs_btn_XacNhan.BackColor = System.Drawing.Color.Lime;
+            this.cs_btn_XacNhan.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.cs_btn_XacNhan.Location = new System.Drawing.Point(219, 534);
+            this.cs_btn_XacNhan.Name = "cs_btn_XacNhan";
+            this.cs_btn_XacNhan.Size = new System.Drawing.Size(246, 64);
+            this.cs_btn_XacNhan.TabIndex = 16;
+            this.cs_btn_XacNhan.Text = "Xác Nhận";
+            this.cs_btn_XacNhan.UseVisualStyleBackColor = false;
+            this.cs_btn_XacNhan.Click += new System.EventHandler(this.cs_btn_XacNhan_Click);
+            // 
+            // cs_btn_Huy
+            // 
+            this.cs_btn_Huy.Location = new System.Drawing.Point(565, 534);
+            this.cs_btn_Huy.Name = "cs_btn_Huy";
+            this.cs_btn_Huy.Size = new System.Drawing.Size(226, 64);
+            this.cs_btn_Huy.TabIndex = 17;
+            this.cs_btn_Huy.Text = "Hủy";
+            this.cs_btn_Huy.UseVisualStyleBackColor = true;
+            // 
+            // cs_dtp_NgaySinh
+            // 
+            this.cs_dtp_NgaySinh.Location = new System.Drawing.Point(179, 222);
+            this.cs_dtp_NgaySinh.Name = "cs_dtp_NgaySinh";
+            this.cs_dtp_NgaySinh.Size = new System.Drawing.Size(245, 27);
+            this.cs_dtp_NgaySinh.TabIndex = 18;
+            // 
+            // CreateStaff
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(963, 754);
+            this.Controls.Add(this.cs_dtp_NgaySinh);
+            this.Controls.Add(this.cs_btn_Huy);
+            this.Controls.Add(this.cs_btn_XacNhan);
+            this.Controls.Add(this.cs_tb_DiaChi);
+            this.Controls.Add(this.cs_lb_DiaChi);
+            this.Controls.Add(this.cs_cb_ChucVu);
+            this.Controls.Add(this.cs_lb_ChucVu);
+            this.Controls.Add(this.cs_cb_BangCap);
+            this.Controls.Add(this.cs_lb_BangCap);
+            this.Controls.Add(this.cs_cb_BoPhan);
+            this.Controls.Add(this.cs_lb_BoPhan);
+            this.Controls.Add(this.cs_lb_Birthday);
+            this.Controls.Add(this.cs_lb_Phone);
+            this.Controls.Add(this.cs_tb_SDT);
+            this.Controls.Add(this.cs_tb_HovaTen);
+            this.Controls.Add(this.c_s_lbHovaTen);
+            this.Controls.Add(this.lbTitle);
+            this.Name = "CreateStaff";
+            this.Text = "CreateCardReader";
+            this.Load += new System.EventHandler(this.CreateStaff_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.Label c_s_lbHovaTen;
+        private System.Windows.Forms.TextBox cs_tb_HovaTen;
+        private System.Windows.Forms.TextBox cs_tb_SDT;
+        private System.Windows.Forms.Label cs_lb_Phone;
+        private System.Windows.Forms.Label cs_lb_Birthday;
+        private System.Windows.Forms.Label cs_lb_BoPhan;
+        private System.Windows.Forms.ComboBox cs_cb_BoPhan;
+        private System.Windows.Forms.Label cs_lb_BangCap;
+        private System.Windows.Forms.ComboBox cs_cb_BangCap;
+        private System.Windows.Forms.Label cs_lb_ChucVu;
+        private System.Windows.Forms.ComboBox cs_cb_ChucVu;
+        private System.Windows.Forms.Label cs_lb_DiaChi;
+        private System.Windows.Forms.TextBox cs_tb_DiaChi;
+        private System.Windows.Forms.Button cs_btn_XacNhan;
+        private System.Windows.Forms.Button cs_btn_Huy;
+        private System.Windows.Forms.DateTimePicker cs_dtp_NgaySinh;
+    }
+}
