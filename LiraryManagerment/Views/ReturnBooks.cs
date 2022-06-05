5fff589ef470f09ff9b889212f6662c4eeb51a3c
HarryThanhDo <harry.thanh@namtech.sg>
2022-06-05 09:43:56 +0700

    fix retuen book

--- LiraryManagerment/Views/ReturnBooks.cs
+++ LiraryManagerment/Views/ReturnBooks.cs
@@ -99,36 +99,47 @@ namespace LiraryManagerment.Views
             try
             {
                 Phieutra phieutra = new Phieutra();
-                Chitietphieutra chitietphieutra = new Chitietphieutra();
+        
                 phieutra.NgayTra = rb_dtp_NgayTra.Value;
                 phieutra.DocGiaId = Convert.ToInt32(rb_tb_IdDocGia.Text);
                 phieutra.TaoMoi = 1;
                 phieutra.DaXoa = 0;
                 using (var content = new heroku_c5dfe82f5ebcccfContext())
                 {
-                    
-                    int selectedColumnCount = rb_dgv_KiemTraPhieuMuon.Columns.GetColumnCount(DataGridViewElementStates.Selected);
-                    int selectedRowCount = rb_dgv_KiemTraPhieuMuon.Rows.GetRowCount(DataGridViewElementStates.Selected);
-                    if (selectedRowCount > 0)
-                    {
-                        System.Text.StringBuilder sb = new System.Text.StringBuilder();
-                        chitietphieutra.SoNgayMuon = 3;
-                        chitietphieutra.TienPhat = Convert.ToInt32(rb_tb_SoTienPhat.Text);
-                        for (int i = 0; i < selectedRowCount; i++)
-                        {
-                            sb.Append(rb_dgv_KiemTraPhieuMuon.SelectedRows[i].Index.ToString());
-                            sb.Append(Environment.NewLine);
-                            for (int j = 0; j < selectedColumnCount; j++)
-                            {
-                                chitietphieutra.SachId = Convert.ToInt32(rb_dgv_KiemTraPhieuMuon.SelectedColumns[2].Index.ToString());
-                                sb.Append(Environment.NewLine);
-                            }
-                        }
-                    }
-
                     var entity = content.Phieutra;
                     entity.Add(phieutra);
                     content.SaveChanges();
+
+                    Chitietphieutra chitietphieutra = new Chitietphieutra();
+
+                    chitietphieutra.Id = phieutra.Id;
+
+                    chitietphieutra.SachId = 34;
+                    chitietphieutra.TienPhat = Convert.ToInt32(rb_tb_SoTienPhat.Text);
+
+                    var entity2 = content.Chitietphieutra;
+                    entity2.Add(chitietphieutra);
+                    content.SaveChanges();
+
+                    //int selectedColumnCount = rb_dgv_KiemTraPhieuMuon.Columns.GetColumnCount(DataGridViewElementStates.Selected);
+                    //int selectedRowCount = rb_dgv_KiemTraPhieuMuon.Rows.GetRowCount(DataGridViewElementStates.Selected);
+                    //if (selectedRowCount > 0)
+                    //{
+                    //    System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                    //    chitietphieutra.SoNgayMuon = 3;
+                    //    chitietphieutra.TienPhat = Convert.ToInt32(rb_tb_SoTienPhat.Text);
+                    //    for (int i = 0; i < selectedRowCount; i++)
+                    //    {
+                    //        sb.Append(rb_dgv_KiemTraPhieuMuon.SelectedRows[i].Index.ToString());
+                    //        sb.Append(Environment.NewLine);
+                    //        for (int j = 0; j < selectedColumnCount; j++)
+                    //        {
+                    //            chitietphieutra.SachId = Convert.ToInt32(rb_dgv_KiemTraPhieuMuon.SelectedColumns[2].Index.ToString());
+                    //            sb.Append(Environment.NewLine);
+                    //        }
+                    //    }
+                    //}
+
                 }
 
                 string message = "Trả sách thành công!";
